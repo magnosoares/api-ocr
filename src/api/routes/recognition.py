199505b1dffb0042a94ec3aefc0303f867025627
magnosoares <magnosoares@gmail.com>
2025-11-19 17:44:12 -0300

    Separação da lógica do OCR para src/services/ocr_service.py

--- src/api/routes/recognition.py
+++ src/api/routes/recognition.py
@@ -2,11 +2,10 @@
 
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
-from pdf2image import convert_from_bytes
+from src.services.ocr_service import ocr_image, ocr_pdf
 from src.models.schemas import ImageUploadForOCR
 from typing import List, Dict, Any
 import logging
-import pytesseract
 from PIL import Image
 import io
 import re
@@ -17,15 +16,14 @@ router = APIRouter(prefix="/recognition", tags = ["Recognition"])
 def text_from_img_file(file: UploadFile = File(...)):
     # validação simples
     if file.content_type not in ["image/jpeg", "image/png"]:
-        return {"error": "Formato inválido. Use JPEG ou PNG."}
-
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Formato inválido! Use JPEG ou PNG."}
+        )
     # leitura da imagem
     image_bytes = file.file.read()
-    image = Image.open(io.BytesIO(image_bytes))
-    
     # processamento OCR
-    texto = pytesseract.image_to_string(image, lang="por")
-    
+    texto = ocr_image(image_bytes)
     return {"text": texto}
 
 
@@ -33,15 +31,14 @@ def text_from_img_file(file: UploadFile = File(...)):
 def text_from_zip_files(file: UploadFile = File(...)):
     # validação simples
     if file.content_type not in ["image/jpeg", "image/png"]:
-        return {"error": "Formato inválido. Use JPEG ou PNG."}
-
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Formato inválido! O arquivo deve ser um ZIP."}
+        )
     # leitura da imagem
     image_bytes = file.file.read()
-    image = Image.open(io.BytesIO(image_bytes))
-    
     # processamento OCR
-    texto = pytesseract.image_to_string(image, lang="por")
-    
+    texto = ocr_image(image_bytes)
     return {"text": texto}
 
 @router.post("/pdf-file")
@@ -50,23 +47,11 @@ def text_from_pdf_file(file: UploadFile = File(...)):
     if file.content_type != "application/pdf":
         return JSONResponse(
             status_code=400,
-            content={"error": "O arquivo deve ser um PDF"}
+            content={"error": "Formato inválido! O arquivo deve ser um PDF."}
         )
-
     # leitura do conteúdo do pdf
     pdf_bytes = file.file.read()
-
-    # Converte cada página em imagem
-    pages = convert_from_bytes(pdf_bytes)
-
-    results: List[Dict[str, Any]] = []
-    for page_number, page in enumerate(pages, start=1):
-        # Aplica OCR com Tesseract
-        text = pytesseract.image_to_string(page, lang="por")  # 'por' para português
-        results.append({
-            "page": page_number,
-            "text": text.strip()
-            })
-
+    # processamento OCR
+    results = ocr_pdf(pdf_bytes)
     return {"ocr_result": results}
 

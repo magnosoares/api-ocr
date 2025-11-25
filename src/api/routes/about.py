34284aea9ed7c4369173bbcb015910e11c9324db
Tiago Fialho <37958485+tiagoASF@users.noreply.github.com>
2025-11-25 20:23:36 -0300

    refact: Update developer names and registrations in about.py

--- src/api/routes/about.py
+++ src/api/routes/about.py
@@ -13,7 +13,7 @@ def about():
     """
     return [
         {"Nome": "Tiago André da Silveira Fialho", "Matrícula": "192.028-6"},
-        {"Nome": "Magno", "Matrícula": "123456"},
+        {"Nome": "Magno Santana Soares", "Matrícula": "1.721.980-9"},
         {"Nome": "Lúcia Helena Dutra Magalhães", "Matrícula": "76.847-2"},
-        {"Nome": "Thiago Lobo", "Matrícula": "45678"},
+        {"Nome": "Thiago Lobo Leite", "Matrícula": "236.128-0"},
     ]
